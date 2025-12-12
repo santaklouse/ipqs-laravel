8de3fb1460687646104a28fbc2237a60bc24e9a3
Alex N <alexnevpryaga@gmail.com>
2025-12-12 16:24:00 +0200

    📝 (Ipqs.php): add method documentation for checkIp, verifyEmail, validatePhone, and bulkValidateCsv to improve code clarity and usability

--- src/Facades/Ipqs.php
+++ src/Facades/Ipqs.php
@@ -3,8 +3,13 @@
 namespace IpqsLaravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use IpqsLaravel\IpqsService;
 
 /**
+ * @method static array checkIp(string $ip, ?string $userAgent = NULL, ?string $userLanguage = NULL, ?int $strictness = NULL)
+ * @method static array verifyEmail(string $email, array $options = [])
+ * @method static array validatePhone(string $phone, array $options = [])
+ * @method static array bulkValidateCsv(string $type, array $list)
  * @see \IpqsLaravel\IpqsService
  */
 class Ipqs extends Facade
